9fe95df43ce34bfd0a65d2beaf9fde7d57640cec
Hieu Nguyen <hieu.nguyen@ericsson.com>
2023-12-25 22:23:19 +0100

    install: Checking to platform and install accordingly

--- install.sh
+++ install.sh
@@ -2,9 +2,25 @@
 
 set -euxo pipefail
 
-echo "Install stow..."
-sudo apt-get update
-sudo apt-get install stow
+export PLATFORM=$(uname -s)
+
+if [ "$PLATFORM" = Linux ]; then
+   sudo apt-get update
+   sudo apt-get install -y stow
+
+   mkdir --parent ~/.fonts
+
+   if [ ! -f ~/.fonts/Go-Mono.tar.xz ]; then
+    curl -L -o ~/.fonts/Go-Mono.tar.xz \
+        https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Go-Mono.tar.xz
+    tar xvf ~/.fonts/Go-Mono.tar.xz -C ~/.fonts/
+    sudo fc-cache -fv
+   fi
+elif [ "$PLATFORM" = Darwin ]; then
+    brew install stow
+    brew tap homebrew/cask-fonts
+    brew install font-hack-nerd-font
+fi
 
 echo "Fetch all git submodules..."
 git submodule update --recursive --init
