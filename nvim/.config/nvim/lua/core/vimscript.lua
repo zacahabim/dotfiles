5bba7a078e9fd8820735fcebe796aadcd59bd800
Hieu Nguyen <zacahabim@gmail.com>
2024-03-08 11:58:06 +0100

    nvim: Use neotree instead of netrw

--- nvim/.config/nvim/lua/core/vimscript.lua
+++ nvim/.config/nvim/lua/core/vimscript.lua
@@ -39,7 +39,7 @@ vim.api.nvim_exec([[
 let g:netrw_keepdir = 0
 let g:netrw_preview = 1
 let g:netrw_alto = 0
-let g:netrw_liststyle = 3
+let g:netrw_liststyle = 0
 let g:netrw_winsize = 30
 let g:netrw_localcopydircmd = 'cp -r'
 let g:netrw_localrmdir='rm -r'
