affabbd8a1d4d12dc50b5e0a0a573cfa2ce71a14
Hieu Nguyen <zacahabim@gmail.com>
2025-01-09 12:18:53 +0100

    Update many things

--- nvim/.config/nvim/lua/plugins/seoul256.lua
+++ nvim/.config/nvim/lua/plugins/seoul256.lua
@@ -1,8 +1,4 @@
 return {
 	"junegunn/seoul256.vim",
 	name = "seoul256",
-	config = function()
-	    vim.cmd('let g:seoul256_background = 235')
-	    vim.cmd('silent! colorscheme seoul256')
-  	end,
 }
