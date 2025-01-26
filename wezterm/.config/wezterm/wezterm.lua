a590d8fd592c79972814da864d81f580cf7dd542
Hieu Nguyen <zacahabim@gmail.com>
2025-01-26 02:17:39 +0200

    wezterm: Disable ctrl-_ so that we can use it in emacs for undo

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,13 +9,19 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'AdventureTime'
 config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 11
+config.font_size = 12
 config.window_background_opacity = 1.0
 config.text_background_opacity = 1.0
 config.warn_about_missing_glyphs = false
 config.enable_scroll_bar = true
 config.exit_behavior = "CloseOnCleanExit"
+config.keys = {
+  {
+    key = '_',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
 
 -- and finally, return the configuration to wezterm
 return config
-
