1622776817cfcd8523d5112b255514b29c858d9d
Hieu Nguyen <zacahabim@gmail.com>
2025-02-08 09:23:14 +0200

    wezterm: Ignore more keybindings
    - org-mode m-enter
    - pop-global-marks

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'AdventureTime'
 config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 12
+config.font_size = 11
 config.window_background_opacity = 1.0
 config.text_background_opacity = 1.0
 config.warn_about_missing_glyphs = false
@@ -19,6 +19,16 @@ config.keys = {
   {
     key = '_',
     mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'Enter',
+    mods = 'ALT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = '@',
+    mods = 'CTRL',
     action = wezterm.action.DisableDefaultAssignment,
   },
 }
