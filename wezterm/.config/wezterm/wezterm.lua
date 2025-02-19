37eda063585655e38f19b7c4be9f621418fd5280
Hieu Nguyen <zacahabim@gmail.com>
2025-02-19 08:40:04 +0200

    wezterm: More configuration for MacOS
    Font size -> 14. Works better in mac os.
    Ignore more keybinding causing mistakes in mac os
    Remove closeoncleanexit

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,12 +9,11 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'AdventureTime'
 config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 11
+config.font_size = 14
 config.window_background_opacity = 1.0
 config.text_background_opacity = 1.0
 config.warn_about_missing_glyphs = false
 config.enable_scroll_bar = true
-config.exit_behavior = "CloseOnCleanExit"
 config.keys = {
   {
     key = '_',
@@ -29,6 +28,41 @@ config.keys = {
   {
     key = '@',
     mods = 'CTRL',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'Tab',
+    mods = 'CTRL',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'Tab',
+    mods = 'CTRL | SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'n',
+    mods = 'CMD',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'p',
+    mods = 'CMD',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'f',
+    mods = 'CMD',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = 'b',
+    mods = 'CMD',
     action = wezterm.action.DisableDefaultAssignment,
   },
 }
